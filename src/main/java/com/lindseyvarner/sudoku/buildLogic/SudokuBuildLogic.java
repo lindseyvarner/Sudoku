630a60fdd1a9239411375498b1d9fa6773378474
Lindsey Varner <lpvarner@gmail.com>
2022-12-25 06:46:20 -0500

    Revert "Update user interface and build logic"
    This reverts commit 43498722773176f0955814d112c517856591dfd5.

--- src/main/java/com/lindseyvarner/sudoku/buildLogic/SudokuBuildLogic.java
+++ src/main/java/com/lindseyvarner/sudoku/buildLogic/SudokuBuildLogic.java
@@ -11,8 +11,15 @@ import java.io.IOException;
 
 public class SudokuBuildLogic {
     public static void build(IUserInterfaceContract.View userInterface) throws IOException {
-        SudokuGame initialState = GameLogic.getNewGame();
+        SudokuGame initialState;
         IStorage storage = new LocalStorageImpl();
+
+        try {
+            initialState = storage.getGameData();
+        } catch (IOException e) {
+            initialState = GameLogic.getNewGame();
+            storage.updateGameData(initialState);
+        }
 
         IUserInterfaceContract.EventListener uiLogic = new ControlLogic(storage, userInterface);
         userInterface.setListener(uiLogic);
--- src/main/java/com/lindseyvarner/sudoku/userInterface/UserInterfaceImpl.java
+++ src/main/java/com/lindseyvarner/sudoku/userInterface/UserInterfaceImpl.java
@@ -33,8 +33,8 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
     private static final double BOARD_PADDING = 50;
     private static final double BOARD_X_AND_Y = 576;
 
-    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(95, 75, 102);
-    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(239, 247, 246);
+    private static final Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 150, 136);
+    private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
     private static final String SUDOKU = "Sudoku";
 
     public UserInterfaceImpl(Stage stage) {
@@ -61,7 +61,7 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
             if (index == 2 || index == 5) {
                 thickness = 3;
             } else {
-                thickness = 1;
+                thickness = 2;
             }
 
             Rectangle verticalLine = getLine(
@@ -94,7 +94,7 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
         line.setY(y);
         line.setHeight(height);
         line.setWidth(width);
-        line.setFill(Color.rgb(95, 75, 102));
+        line.setFill(Color.BLACK);
 
         return line;
     }
@@ -145,9 +145,9 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
     }
 
     private void drawTitle(Group root) {
-        Text title = new Text(280, 690, SUDOKU);
-        title.setFill(Color.rgb(239, 247, 246));
-        Font titleFont = new Font("serif italic",42);
+        Text title = new Text(235, 690, SUDOKU);
+        title.setFill(Color.WHITE);
+        Font titleFont = new Font(43);
         title.setFont(titleFont);
         root.getChildren().add(title);
     }
@@ -196,10 +196,9 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View, EventHand
                 if (game.getGameState() == GameState.NEW) {
                     if (value.equals("")) {
                         tile.setStyle("-fx-opacity: 1;");
-                        //tile.setStyle("-fx-text-fill: rgb(95, 75, 102);");
                         tile.setDisable(false);
                     } else {
-                        tile.setStyle("-fx-text-fill: rgb(95, 75, 102);");
+                        tile.setStyle("-fx-opacity: 0.8;");
                         tile.setDisable(true);
                     }
                 }
